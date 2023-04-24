387716dd51276e982d64016423093f287132ed32
Eduardo Vergara <eduver_san@hotmail.com>
2023-04-24 16:42:56 -0500

    feat: :sparkles: Add currency conversion

--- src/db/data.js
+++ src/db/data.js
@@ -1,3 +1,12 @@
 export const siteUrl = 'http://localhost:3000/';
 export const salaryCOP = 1_160_000;
 export const salaryEUR = 760;
+
+export const currencies = Object.freeze({
+  COP: 'COP',
+  EUR: 'EUR',
+});
+
+export const API_KEY = '17bd8698cc8eae5174abb0c96fd42cf526d3e522';
+export const ENDPOINT =
+  'https://api.getgeoapi.com/v2/currency/convert?api_key=17bd8698cc8eae5174abb0c96fd42cf526d3e522';
